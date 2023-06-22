37832e3fd85da7c1989506bf5952903bde9c5c1f
lpcastilla <128614607+lpcastilla@users.noreply.github.com>
2023-06-22 00:24:39 +0000

    update navbar

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -19,21 +19,5 @@ export let NavBar = function () {
           </div>
       </div>
   </nav>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary container" style="background-color: black;">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#" style="color: white">Start bootstrap</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup" style="justify-content: flex-end; display: flex;">
-                  <div className="navbar-nav">
-                      <a className="nav-link active" style="color: white" aria-current="page" href="#">Home</a>
-                      <a className="nav-link active" style="color: white" href="#">About</a>
-                      <a className="nav-link active" style="color: white" href="#">Services</a>
-                      <a className="nav-link active" style="color: white" href="#">Contact</a>
-                  </div>
-              </div>
-        </div>
-    </nav>
   )
 }
