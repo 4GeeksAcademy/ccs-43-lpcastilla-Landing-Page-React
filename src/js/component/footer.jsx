eb0ef848219ced526124023beadd3d6056dbec77
lpcastilla <128614607+lpcastilla@users.noreply.github.com>
2023-06-22 00:58:28 +0000

    se agrego el footer

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export let Footer = function () {
+    return (
+        <div class="card-footer bg-dark mt-4 p-0 wd-100">
+            Copyright © Your Website 2023
+        </div>
+    )
+}
+
+
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,17 +3,18 @@ import React from "react";
 //include images into your bundle
 import {Jumbotron} from "./jumbotron.jsx"
 import {NavBar} from "./Navbar.jsx"
-
+import {Footer} from "./footer.jsx"
 
 import  {Card}  from "./card.jsx";
 
 //create your first component
 export const Home = () => {
 	return (
-		<div className="container">
+		<div className="container-fluid p-0">
 			<NavBar />
 			<Jumbotron />
 			<Card />
+			<Footer />
 		</div>
 	);
 };
