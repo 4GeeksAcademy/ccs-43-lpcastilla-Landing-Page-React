a9dcf2f6bef51089aa94db10bdf2013955171277
jerinhons117 <131413483+jerinhons117@users.noreply.github.com>
2023-06-22 02:26:51 +0000

    agrego cards en filas terminados 10:26pm

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -7,32 +7,44 @@ import ReactDOM from "react-dom";
 export const Card = function () {
     return (
         <div className="container">
-            <div className="card-group">
-                <div className="card">
-                    <img src="..." className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                    <h5 className="card-title">Título de la tarjeta</h5>
-                    <p className="card-text">Esta es una tarjeta más amplia con texto de apoyo a continuación como introducción natural a contenido adicional. Este contenido es un poco más largo.</p>
-                    <p className="card-text"><small className="text-muted">Última actualización hace 3 minutos</small></p>
-                    </div>
+          <div className="row row-cols-1 row-cols-md-3 g-4">
+            <div className="col">
+                <div className="card h-100">
+                <img src="https://img.freepik.com/free-vector/colourful-illustration-programmer-working_23-2148281410.jpg?q=10&h=200" className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">Leonardo: desarrollador Full-Stack</h5>
+                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 </div>
-                <div className="card">
-                    <img src="..." className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                    <h5 className="card-title">Título de la tarjeta</h5>
-                    <p className="card-text">Esta tarjeta tiene texto de apoyo a continuación como una introducción natural a contenido adicional.</p>
-                    <p className="card-text"><small className="text-muted">Última actualización hace 3 minutos</small></p>
-                    </div>
-                </div>
-                <div className="card">
-                    <img src="..." className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                    <h5 className="card-title">Título de la tarjeta</h5>
-                    <p className="card-text">Esta es una tarjeta más amplia con texto de apoyo a continuación como introducción natural a contenido adicional. Esta tarjeta tiene un contenido aún más largo que la primera para mostrar esa acción de igual altura.</p>
-                    <p className="card-text"><small class="text-muted">Última actualización hace 3 minutos</small></p>
-                    </div>
+                <div className="card-footer">
+                <a href="#" className="btn btn-secondary">Mas informacion sobre leonardo</a>
                 </div>
                 </div>
+            </div>
+            <div className="col">
+                <div className="card h-100">
+                <img src="https://img.freepik.com/free-vector/coding-concept-illustration_114360-1155.jpg?q=10&h=200" className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">Nicolas: desarrollador Full-Stack</h5>
+                    <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                <div className="card-footer">
+                    <a href="#" className="btn btn-secondary">Mas informacion sobre nicolas</a>
+                </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card h-100">
+                <img src="https://img.freepik.com/free-vector/hacker-operating-laptop-cartoon-icon-illustration-technology-icon-concept-isolated-flat-cartoon-style_138676-2387.jpg?desarrollador Full-Stack" className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">Jesus: desarrollador Full-Stack</h5>
+                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                </div>
+                <div className="card-footer">
+                    <a href="#" className="btn btn-secondary">Mas informacion sobre jesus</a>
+                </div>
+                </div>
+            </div>
+            </div>
         </div>
     
     )
