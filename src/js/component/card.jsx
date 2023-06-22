6eb58a06cab5ac8e5aac120bd9b6ffdfe00997d0
jerinhons117 <131413483+jerinhons117@users.noreply.github.com>
2023-06-22 01:45:09 +0000

    agrego card en fila

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -6,38 +6,34 @@ import ReactDOM from "react-dom";
 
 export const Card = function () {
     return (
-            <ul className="row">
-                <li>
-                <div className="card" style={{width: '18rem'}}>
+        <div className="container">
+            <div className="card-group">
+                <div className="card">
                     <img src="..." className="card-img-top" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <h5 className="card-title">Título de la tarjeta</h5>
+                    <p className="card-text">Esta es una tarjeta más amplia con texto de apoyo a continuación como introducción natural a contenido adicional. Este contenido es un poco más largo.</p>
+                    <p className="card-text"><small className="text-muted">Última actualización hace 3 minutos</small></p>
                     </div>
                 </div>
-                </li>
-                <li>
-                <div className="card" style={{width: '18rem'}}>
+                <div className="card">
                     <img src="..." className="card-img-top" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <h5 className="card-title">Título de la tarjeta</h5>
+                    <p className="card-text">Esta tarjeta tiene texto de apoyo a continuación como una introducción natural a contenido adicional.</p>
+                    <p className="card-text"><small className="text-muted">Última actualización hace 3 minutos</small></p>
                     </div>
                 </div>
-                </li>
-                <li>
-                <div className="card" style={{width: '18rem'}}>
+                <div className="card">
                     <img src="..." className="card-img-top" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <h5 className="card-title">Título de la tarjeta</h5>
+                    <p className="card-text">Esta es una tarjeta más amplia con texto de apoyo a continuación como introducción natural a contenido adicional. Esta tarjeta tiene un contenido aún más largo que la primera para mostrar esa acción de igual altura.</p>
+                    <p className="card-text"><small class="text-muted">Última actualización hace 3 minutos</small></p>
                     </div>
                 </div>
-                </li>
-            </ul>
+                </div>
+        </div>
     
     )
     
