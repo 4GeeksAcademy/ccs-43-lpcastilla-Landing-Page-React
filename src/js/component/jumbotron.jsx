61b02b96f2081fff43b324d85d061e3fa8139648
lpcastilla <128614607+lpcastilla@users.noreply.github.com>
2023-06-22 14:39:25 +0000

    updates de jumbotron y footer

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 
 export let Jumbotron = function () {
     return (
-        <div className="card bg-light m-4">
-            <div className="card-body">
-              <h1 className="card-title">A Warm Welcome!</h1>
-              <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, maiores, delectus cum eligendi sunt distinctio, quisquam quidem similique obcaecati magni veniam eos perferendis cumque iste. Assumenda autem delectus tempora molestiae.</p>
-              <a href="#" className="btn btn-primary">Call to action!</a>
+        <div className="container-fluid mx-auto m-0 p-0" style={{ width:'85%'}}>
+            <div className="card bg-light m-4">
+                <div className="card-body">
+                <h1 className="card-title">A Warm Welcome!</h1>
+                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, maiores, delectus cum eligendi sunt distinctio, quisquam quidem similique obcaecati magni veniam eos perferendis cumque iste. Assumenda autem delectus tempora molestiae.</p>
+                <a href="#" className="btn btn-primary">Call to action!</a>
+                </div>
             </div>
         </div>
     )
